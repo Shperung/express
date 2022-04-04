e80251fa3ba7da942d1c38e358e6c600ddfe213b
Viktor Kravchuk <viktorkrav4uk87@gmail.com>
2022-04-04 22:06:41 +0300

    Add index.html

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/express/', (req, res) => {
-	res.send('<h1>Hello API3</h1>');
+	//res.send('<h1>Hello Express.</h1>');
 });
 
 app.listen(port, () => {
