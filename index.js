a4c43c82f853d5c6be179e07f944142d3ccfb642
Viktor Kravchuk <viktorkrav4uk87@gmail.com>
2022-04-04 21:40:28 +0300

    init express

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+	res.send('<h1>Hello Express</h1>');
+});
+
+app.listen(port, () => {
+  console.log(`API starded in http://localhost:${port}/ .......`);
+});
