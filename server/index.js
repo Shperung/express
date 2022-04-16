444e7e943df0296250c2b7a3b021630a11f13d0f
Viktor Kravchuk <viktorkrav4uk87@gmail.com>
2022-04-16 18:43:02 +0300

    Add react

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 import express from "express";
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.get('/express/', (req, res) => {
-	//res.send('<h1>Hello Express.</h1>');
+	res.send('<h1>Hello Express.</h1>');
 });
 
 app.listen(port, () => {
