4662e78c52871a261602f5c1317498618ff91fa8
Viktor Kravchuk <viktorkrav4uk87@gmail.com>
2022-04-16 21:21:42 +0300

    add graphQl

--- server/index.js
+++ server/index.js
@@ -1,10 +1,44 @@
 import express from "express";
-const app = express();
+import { graphqlHTTP } from "express-graphql";
+import cors from "cors";
+
+import schema from "./schema.mjs";
+import users from "./users.mjs";
 
 const port = process.env.PORT || 3001;
+const app = express();
+app.use(cors());
 
-app.get('/express/', (req, res) => {
-	res.send('<h1>Hello Express.</h1>');
+const createUser = (input) => {
+  const id = Date.now();
+  return {
+    id,
+    ...input,
+  };
+};
+
+const root = {
+  getAllUsers: () => users,
+  getUser: ({ id }) => users.find((user) => user.id == id),
+  createUser: ({ input }) => {
+    const user = createUser(input);
+    users.push(user);
+    return user;
+  },
+};
+
+// візуальна діч для graphq
+app.use(
+  "/express/graphgl",
+  graphqlHTTP({
+    graphiql: true,
+    schema,
+    rootValue: root,
+  })
+);
+
+app.get("/express", (req, res) => {
+  res.send("<h1>Hello Express.</h1>");
 });
 
 app.listen(port, () => {
---
+++ server/users.mjs
@@ -0,0 +1,32 @@
+const users = [
+  {
+    id: 1,
+    username: "User 1",
+    age: 25,
+    posts: [
+      {
+        id: 1,
+        title: "title 1 - 1",
+        content: "content 1 - 1",
+      },
+      {
+        id: 2,
+        title: "title 1 - 2",
+        content: "content 1 - 2",
+      },
+    ],
+  },
+  {
+    id: 2,
+    username: "User 2",
+    age: 24,
+    posts: [
+      {
+        id: 1,
+        title: "title 2 - 1",
+        content: "content 2 - 1",
+      },
+    ],
+  },
+];
+export default users;
